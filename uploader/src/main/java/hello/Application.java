f72da5fed64fe2a619f04c9e143f14e2e8a5cc6d
Manuel <darkpriestrelative@gmail.com>
2016-08-10 22:47:12 -0400

    Uploader module upload

---
+++ uploader/src/main/java/hello/Application.java
@@ -0,0 +1,31 @@
+package hello;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.FileSystemUtils;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * Created by Manuel Perez P. (darkpriestrelative@gmail.com) on 10/08/16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init() {
+        return (args) -> {
+            FileSystemUtils.deleteRecursively(new File(FileUploadController.ROOT));
+
+            Files.createDirectory(Paths.get(FileUploadController.ROOT));
+        };
+    }
+}
